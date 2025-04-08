dc92da5beea072a3488e1e9f0e17e14153c27b92
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-04-08 14:11:24 +0000

    index

--- my_reactApp/my_reactApp/src/main.jsx
+++ my_reactApp/my_reactApp/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'; from './src'
 import App  from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
