f9e9ffe704b7280d2fac87d262d4b9a726bb97f3
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-04-08 17:25:36 +0000

    App.jsx

--- my_reactApp/my_reactApp/src/App.jsx
+++ my_reactApp/my_reactApp/src/App.jsx
@@ -6,9 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
+    return (
+       <div>
    //   <a href="https://vite.dev" target="_blank">
       //  <img src={viteLogo} className="logo" alt="Vite logo" />
       //</a>
@@ -17,43 +16,90 @@ function App() {
       // </a>
   </div> 
       <div>
-	<h1>My To Do List</h1>
+        <h1>My To Do List</h1>
       </div>
-<div className="">
-        <button OnClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <script>
-	    function AddItem() {
-		const Input = document.getElementById("itemInput");
-		 const Value = input.value.trim();
-		  if (value === "") return;
+//<div className="">
+  //      <button OnClick={() => setCount((count) => count + 1)}>
+  //        count is {count}
+//        </button>
+ //       <p>
+  //        <script>
+//            function AddItem() {
+//                const Input = document.getElementById("itemInput");
+//                 const Value = input.value.trim();
+//                  if (value === "") return;
 
-		  const Li = document.createElement("li");
-		  li.className = "list-item";
-		  li.textContent = value;
+  //                const Li = document.createElement("li");
+ //                 li.className = "list-item";
+ //                 li.textContent = value;
 
-		  document.getElementById("itemList").appendChild(li);
-		  input.value = "";
-	      }
-	  </script>
-        </p>
-      </div>
-      <p className="">
+     //             document.getElementById("itemList").appendChild(li);
+   //               input.value = "";
+     //         }
+    //      </script>
+  //      </p>
+//      </div>
+//      <p className="">
   
       </p>
       <div>
-	AddItem
-      </div>
-      <div>
-        Input
-      </div>
-      <div>
-	Value
-      </div>
-Li
+       < AddItem />
+      < Input />
+      <  Value />
+<Li />
+
+      
+
+
+
 </div>
+      
+  )
+}
+
+
+export default App
+
+        <div>
+   //   <a href="https://vite.dev" target="_blank">
+      //  <img src={viteLogo} className="logo" alt="Vite logo" />
+      //</a>
+   //<a href="https://react.dev" target="_blank">
+      //  <img src={reactLogo} className="logo react" alt="React logo" />
+      // </a>
+  </div> 
+      <div>
+        <h1>My To Do List</h1>
+      </div>
+//<div className="">
+  //      <button OnClick={() => setCount((count) => count + 1)}>
+  //        count is {count}
+//        </button>
+ //       <p>
+  //        <script>
+//            function AddItem() {
+//                const Input = document.getElementById("itemInput");
+//                 const Value = input.value.trim();
+//                  if (value === "") return;
+
+  //                const Li = document.createElement("li");
+ //                 li.className = "list-item";
+ //                 li.textContent = value;
+
+     //             document.getElementById("itemList").appendChild(li);
+   //               input.value = "";
+     //         }
+    //      </script>
+  //      </p>
+//      </div>
+//      <p className="">
+  
+      </p>
+      <div>
+       < AddItem />
+      < Input />
+      <  Value />
+<Li />
 
       
 
