60b390654a679ab68f0b0bba87e4ae0fde1f423d
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-04-08 18:06:22 +0000

    App.jsx

--- my_reactApp/my_reactApp/src/App.jsx
+++ my_reactApp/my_reactApp/src/App.jsx
@@ -33,63 +33,10 @@ function App() {
 //      <p className="">
   
       <div>
-       < AddItem />
-      < Input />
-      <  Value />
+      <AddItem />
+      <Input />
+      <Value />
       <Li />
-
-      
-
-
-
-</div>
-      
-  )
-}
-
-
-export default App
-
-        <div>
-   //   <a href="https://vite.dev" target="_blank">
-      //  <img src={viteLogo} className="logo" alt="Vite logo" />
-      //</a>
-   //<a href="https://react.dev" target="_blank">
-      //  <img src={reactLogo} className="logo react" alt="React logo" />
-      // </a>
-  </div> 
-      <div>
-        <h1>My To Do List</h1>
-      </div>
-//<div className="">
-  //      <button OnClick={() => setCount((count) => count + 1)}>
-  //        count is {count}
-//        </button>
- //       <p>
-  //        <script>
-//            function AddItem() {
-//                const Input = document.getElementById("itemInput");
-//                 const Value = input.value.trim();
-//                  if (value === "") return;
-
-  //                const Li = document.createElement("li");
- //                 li.className = "list-item";
- //                 li.textContent = value;
-
-     //             document.getElementById("itemList").appendChild(li);
-   //               input.value = "";
-     //         }
-    //      </script>
-  //      </p>
-//      </div>
-//      <p className="">
-  
-      </p>
-      <div>
-       < AddItem />
-      < Input />
-      <  Value />
-<Li />
 
       
 
