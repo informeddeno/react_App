1e3cc2f6d81704bda17109916163b4e3a7857bc0
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-04-21 09:54:13 +0000

    App.jsx

--- my_reactApp/src/main.jsx
+++ my_reactApp/src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./public/index.html";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
  root.render(
-  <React.StrictMode>
+  <react.StrictMode>
     <App />
-  </React.StrictMode>
+  </react.StrictMode>
 );
