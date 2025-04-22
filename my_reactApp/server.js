0362ad3075f732ff38ada0405281fa5613976aed
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-04-22 16:54:22 +0000

    package.json

---
+++ my_reactApp/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve the static files from the React app (build directory)
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handles any requests that don't match the static files
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
