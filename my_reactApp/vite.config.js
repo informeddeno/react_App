7eb80990d21ec39b5ebaf892541109fca90438ba
informeddeno <125739814+informeddeno@users.noreply.github.com>
2025-05-01 13:12:01 +0000

    vit.config.js

--- my_reactApp/vite.config.js
+++ my_reactApp/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensure the base path is correct
+  build: {
+    outDir: 'build', // Ensure the output directory matches where you serve files
+  },
 })
